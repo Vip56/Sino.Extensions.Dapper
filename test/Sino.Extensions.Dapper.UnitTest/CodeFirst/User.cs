1818e51d3e8ed14a3351bdadb3b985450e673c65
姚正发 <976691141@qq.com>
2018-03-07 15:43:52 +0800

    增加dapper demo

---
+++ test/Sino.Extensions.Dapper.UnitTest/CodeFirst/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sino.Extensions.Dapper
+{
+    public class User
+	{
+		public int Id { get; set; }
+
+		public DateTime CreationTime { get; set; }
+
+		public string UserName { get; set; }
+
+		public Sex Sex { get; set; }
+
+		public virtual IList<Order> Orders { get; set; }
+	}
+
+	public enum Sex
+	{
+		Man,
+		Woman
+	}
+}
