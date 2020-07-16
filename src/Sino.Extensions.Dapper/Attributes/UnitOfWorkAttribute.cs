8da7d155e3d50538cf39cd7d88b0ed9906daa984
刘飞 <251168923@qq.com>
2020-07-16 14:24:29 +0800

    通过AOP实现全局事务

---
+++ src/Sino.Extensions.Dapper/Attributes/UnitOfWorkAttribute.cs
@@ -0,0 +1,37 @@
+﻿using AspectCore.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Sino.Extensions.Dapper.Attributes
+{
+    /// <summary>
+    /// 工作单元
+    /// </summary>
+    public class UnitOfWorkAttribute : AbstractInterceptorAttribute
+    {
+        public async override Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            try
+            {
+                using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    await next(context);
+                    scope.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                //判断异常类型 如果是Sino异常则进行转发
+                if (ex is SinoException)
+                {
+                    var sex = ex as SinoException;
+                    throw new SinoException(sex.Message, sex.Code);
+                }
+                throw new Exception(ex.StackTrace);
+            }
+        }
+    }
+}
